db88e640344bde2ee762b7a8243b00ed9b62093c
Jacob McClure <jacobmcclure801@gmail.com>
2020-12-05 15:10:22 -0700

    commit on master

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-alert("Hello!")
+//get modal
+let modal = document.getElementById("myModal");
+//get button that opens modal
+let openModal = document.querySelectorAll(".openModal");
+//get span element that closes modal
+let span = document.getElementsByClassName("close")[0];
+
+//assigns event listener to all class items
+for (i = 0; i < openModal.length; i++) {
+    openModal[i].addEventListener("click", expandImg)
+}
+
+//button press opens modal
+function expandImg(event) {
+    let image = event.target;
+    modal.style.display = "block";
+    image.src = this.src;
+    console.log(image.src);
+}
+//X "span" click closes modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+//User clicks outside of modal it'll close
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
